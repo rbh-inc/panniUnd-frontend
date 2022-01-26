9f081739f1b5e12ed96af5b7c2431a04a6b08acc
rabeeh-ta <rabeehchemnad@gmail.com>
2022-01-26 23:18:35 +0530

    REFACT: individual components

---
+++ src/components/LandingContent.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="container">
+    <div class="main-section">
+      <div class="landing-title">
+        <h2>
+          Find a <br />
+          <span>freelancer</span>
+        </h2>
+      </div>
+      <div class="landing-description">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus,
+          expedita necessitatibus. Accusamus vitae quis ex cum obcaecati sed
+          soluta labore autem.
+        </p>
+      </div>
+      <div class="landing-btns">
+        <button><router-link to="/about">I am a worker</router-link></button>
+        <button><router-link to="/about">Find a worker</router-link></button>
+      </div>
+    </div>
+    <div class="main-illustration">
+      <img src="../assets/underConstruction.svg" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LandingContent",
+};
+</script>
+
+<style scoped>
+#container {
+  margin: 20px 30px 0 30px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  height: 40%;
+}
+.main-section {
+  width: 50%;
+  margin-top: 40px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.landing-title h2 {
+  font-size: 45px;
+  margin-left: 10px;
+}
+.landing-title span {
+  font-size: 50px;
+  margin-left: 25px;
+}
+
+.main-illustration {
+  width: 45%;
+}
+
+.landing-btns button {
+  border: none;
+  margin: 20px 20px 0px 20px;
+  padding: 10px 20px 10px 20px;
+  font-weight: bold;
+  background-color: #d97356;
+  border: solid 1px #ae5c45;
+  color: white;
+}
+
+.main-illustration img {
+  width: 550px;
+}
+</style>
---
+++ src/components/SectorCategories.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="container">
+    <div class="sector-title">
+      <h1>Sectors</h1>
+    </div>
+
+    <div class="sector-list">
+      <div class="sector-list-card">
+        <img src="https://img.icons8.com/fluency/96/000000/home.png" />
+        <p>House Worker</p>
+      </div>
+      <div class="sector-list-card">
+        <img src="https://img.icons8.com/clouds/50/000000/dinner-hotel.png" />
+        <p>House Worker</p>
+      </div>
+      <div class="sector-list-card">
+        <img
+          src="https://img.icons8.com/external-flatart-icons-flat-flatarticons/50/000000/external-agriculture-agriculture-flatart-icons-flat-flatarticons.png"
+        />
+        <p>House Worker</p>
+      </div>
+      <div class="sector-list-card">
+        <img
+          src="https://img.icons8.com/external-justicon-lineal-color-justicon/50/000000/external-mechanic-avatar-and-occupation-justicon-lineal-color-justicon.png"
+        />
+        <p>House Worker</p>
+      </div>
+      <div class="sector-list-card">
+        <img
+          src="https://img.icons8.com/external-justicon-lineal-color-justicon/50/000000/external-hospital-hospital-justicon-lineal-color-justicon.png"
+        />
+        <p>House Worker</p>
+      </div>
+      <div class="sector-list-card">
+        <img
+          src="https://img.icons8.com/external-becris-flat-becris/50/000000/external-education-literary-genres-becris-flat-becris.png"
+        />
+        <p>House Worker</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SectorCategories",
+};
+</script>
+
+<style scoped>
+.container {
+  background-color: #262c31;
+
+  width: 100%;
+  margin: 20px 0 15px 0;
+  display: flex;
+  flex-direction: column;
+  color: white;
+}
+
+.sector-title {
+  margin-top: 10px;
+  text-align: center;
+}
+
+.sector-list {
+  display: flex;
+  margin: 0px 30px 20px 30px;
+}
+
+.sector-list-card {
+  margin: 2px 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: white;
+  border-radius: 5px;
+  box-shadow: #fbf1ed;
+  height: 130px;
+  width: 130px;
+}
+.sector-list-card img {
+  width: 60px;
+  padding-top: 15px;
+}
+.sector-list-card p {
+  color: #262c31;
+  margin: 10px;
+}
+</style>
+
+
