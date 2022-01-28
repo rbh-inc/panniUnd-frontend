7d46b9fb9c882c33c49f442917717cc05e11ab00
rabeeh-ta <rabeehchemnad@gmail.com>
2022-01-29 00:36:01 +0530

    new components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <LandingContent></LandingContent>
   <SectorCategories></SectorCategories>
+  <Recommendations></Recommendations>
 </template>
 
 <script>
 import LandingContent from "../components/LandingContent.vue";
 import SectorCategories from "../components/SectorCategories.vue";
+import Recommendations from "../components/Recommendation.vue";
 export default {
   name: "Home",
   components: {
     SectorCategories,
     LandingContent,
+    Recommendations,
   },
 };
 </script>
