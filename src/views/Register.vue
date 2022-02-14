3cab7f1b11ff2caa0feeca206937336e532e94e7
rabeeh-ta <rabeehchemnad@gmail.com>
2022-02-14 18:29:53 +0530

    alert() feedback after worker submission

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -188,6 +188,7 @@ export default {
             alert("fill all the forms correctly");
           } else {
             console.log(success);
+            alert("submission succuss");
           }
         })
         .catch((e) => {
